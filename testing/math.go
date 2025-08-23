a39bf34e36d20bd37b9209823df962a27b5f637e
Ayush Chauhan <ayush.c@razorpay.com>
2025-08-23 17:17:25 +0530

    These are the basics of testing

---
+++ testing/math.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func Calculate(val int) int {
+	return val + 2
+}
+func main() {
+	result := Calculate(2)
+	fmt.Println("The value that we got")
+	fmt.Println(result)
+}
