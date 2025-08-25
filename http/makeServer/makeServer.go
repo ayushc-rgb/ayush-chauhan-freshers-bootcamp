c7a3475a303848fbdff114306ccf71b27ad44386
Ayush Chauhan <ayush.c@razorpay.com>
2025-08-25 13:10:24 +0530

    to create an HTTP Endpoint

---
+++ http/makeServer/makeServer.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello there!\n")
+}
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
