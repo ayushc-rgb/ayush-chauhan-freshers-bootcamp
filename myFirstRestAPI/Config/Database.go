5c2b48c14fdbae04924d46f435fe0432fb3cc6aa
Ayush Chauhan <ayush.c@razorpay.com>
2025-08-28 19:51:47 +0530

    this is for the configuration of the database

---
+++ myFirstRestAPI/Config/Database.go
@@ -0,0 +1,32 @@
+package Config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+// this is for the configuration of the database which tells us which database will be used and configured
+var DB *gorm.DB
+
+type DBConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     int
+	DBName   string
+}
+
+func BuildDBConfig() *DBConfig {
+	return &DBConfig{
+		Host:     "127.0.0.1:3306",
+		Port:     3306,
+		User:     "root",
+		Password: "ayush123",
+		DBName:   "apidb",
+	}
+}
+
+func DBURL(db *DBConfig) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", db.User, db.Password, db.Host, db.Port, db.DBName)
+}
